e239f514345aa6db6112a1b1c3d5e7eb54f06de7
hunghx <baby10051999@gmail.com>
2023-11-24 19:05:27 +0700

    up

---
+++ src/ra/run/StudentManagement.java
@@ -0,0 +1,18 @@
+package ra.run;
+
+import ra.business.util.InputMethods;
+
+public class StudentManagement {
+    public static void main(String[] args) {
+        // chạy chương trình
+
+        // b1 điều hướng menu :
+        // công nghệ sử dụng
+        // do while để duy trì ứng dụng
+        // switch case để điều hướng chức năng do người dùng nhập vào
+        // I/O data
+
+        System.out.println("Nhập vào 1 số");
+        int so = InputMethods.getInteger();
+    }
+}
